765f27b83d2a4f51a630cbc8ca3af5f7dc999610
truzcartmain-debug <truzcartmain@gmail.com>
2026-07-21 21:08:13 +0530

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -235,8 +235,8 @@ window.SITE_CONFIG = {
       ],
 
       streamServers: [
-        { label: "Server 1", embedUrl: "", embedCode: "" },
-        { label: "Server 2", embedUrl: "", embedCode: "" }
+        { label: "Server 1", embedUrl: "", embedCode: "<iframe src=\"https://logic.icelanders.st/embed/dazn1-de\" width=\"100%\" height=\"100%\" frameborder=\"0\" scrolling=\"no\" allow=\"autoplay; encrypted-media; picture-in-picture; fullscreen\" allowfullscreen></iframe>" },
+        { label: "Server 2", embedUrl: "", embedCode: "<iframe src=\"https://logic.icelanders.st/embed/tntsports1-uk\" width=\"100%\" height=\"100%\" frameborder=\"0\" scrolling=\"no\" allow=\"autoplay; encrypted-media; picture-in-picture; fullscreen\" allowfullscreen></iframe>" }
       ]
     }
 
